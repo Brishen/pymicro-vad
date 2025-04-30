0cf3540b68085a0bdcb65ba9e8c17add952ea6c9
Brishen <brishen.hawkins@gmail.com>
2025-04-29 23:17:42 -0400

    refactor: Remove pymicro_vad2 package and update setup configuration

--- pymicro_vad2/__init__.py
+++
@@ -1,4 +0,0 @@
-# Import everything from pymicro_vad to make it available under this namespace
-from pymicro_vad import *
-
-# This allows users to import from pymicro_vad even though the package is named pymicro_vad2
